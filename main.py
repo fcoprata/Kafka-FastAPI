d3db93befc4fe6e526c4e519e74a444d54f80435
Francisco Prata Cavalcante Neto <francisco.prata@engdb.com.br>
2022-12-14 11:45:18 -0300

    add router /confirmed

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import json
 from config import KAFKA_BOOSTRAP_SERVERS, ORDER_KAFKA_TOPIC
 from kafka import KafkaProducer
-from fastapi import FastAPI
+from fastapi import FastAPI,APIRouter
 import uvicorn
+import validate_order
 
 app = FastAPI()
 producer = KafkaProducer(bootstrap_servers=KAFKA_BOOSTRAP_SERVERS)
 
+app.include_router(validate_order.router)
+
 @app.post("/order")
-def order(name: str, items: str, value: float, cartao: int):
+def order(name: str, items: str, value: float, cartao: int, cvv: int, email: str):
     data = {
         "name": name,
         "items": items,
         "value": value,
-        "cartao": cartao
+        "cartao": cartao,
+        "cvv": cvv,
+        "email": email
     }
     producer.send(ORDER_KAFKA_TOPIC,json.dumps(data).encode("utf-8"))
     return {"order finished"}
---
+++ validate_order.py
@@ -0,0 +1,36 @@
+from fastapi import APIRouter
+import json
+from kafka import KafkaConsumer, KafkaProducer
+from config import KAFKA_BOOSTRAP_SERVERS, ORDER_KAFKA_TOPIC, ORDER_CONFIRMED_KAFKA_TOPIC
+
+consumer = KafkaConsumer(ORDER_KAFKA_TOPIC,bootstrap_servers=KAFKA_BOOSTRAP_SERVERS)
+producer = KafkaProducer(bootstrap_servers=KAFKA_BOOSTRAP_SERVERS)
+
+router = APIRouter(prefix='/order')
+
+@router.get("/confirmed")
+def order_confirmed():
+    for message in consumer:
+        consumed_message = json.loads(message.value.decode())
+        name = consumed_message["name"]
+        value = consumed_message["value"]
+        cartao = consumed_message["cartao"]
+        cvv = consumed_message["cvv"]
+
+        if len(str(cartao)) == 16:
+            cartao_validado = cartao
+        else:
+            print({"cartão inválido"})
+        if len(str(cvv)) == 3:
+            cvv_validado = cvv
+        else:
+            print({"cvv inválido"})
+
+        data_order_confirmed = {
+            "name": name,
+            "cartao": cartao_validado,
+            "value": value,
+            "cvv": cvv_validado
+        }
+    producer.send(ORDER_CONFIRMED_KAFKA_TOPIC, json.dumps(data_order_confirmed).encode("utf-8"))
+    return {data_order_confirmed}
