f84d322b1c4cf33476457637b63b12b7dd141b20
Francisco Prata <fcopratan@gmail.com>
2022-12-14 22:29:00 -0300

    update main

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import json
 from config import KAFKA_BOOSTRAP_SERVERS, ORDER_KAFKA_TOPIC
 from kafka import KafkaProducer
-from fastapi import FastAPI,APIRouter
+from fastapi import FastAPI
 import uvicorn
 import validate_order
+from datetime import datetime
 
 app = FastAPI()
 producer = KafkaProducer(bootstrap_servers=KAFKA_BOOSTRAP_SERVERS)
@@ -12,13 +13,15 @@ app.include_router(validate_order.router)
 
 @app.post("/order")
 def order(name: str, items: str, value: float, cartao: int, cvv: int, email: str):
+    data_compra = datetime.now()
     data = {
         "name": name,
         "items": items,
         "value": value,
         "cartao": cartao,
         "cvv": cvv,
-        "email": email
+        "email": email,
+        "data": str(data_compra)
     }
     producer.send(ORDER_KAFKA_TOPIC,json.dumps(data).encode("utf-8"))
     return {"order finished"}
